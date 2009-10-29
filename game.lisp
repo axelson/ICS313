fcb4c230cee893a3fd1300f40dc3cc305fc1fab7
Jason Axelson (dell.server) <jason.axelson@gmail.com>
2009-10-28 23:15:51 -1000

    added initializations for containers

--- game.lisp
+++ game.lisp
@@ -31,3 +31,10 @@
   (setf (get 'bart 'age) 7) 
   (setf (get 'bart 'hair-color) 'yellow) 
   (setf (get 'bart 'name) "Bart Simpson"))
+
+(progn
+  (defparameter pouch '((description "a small coin pouch") (contents (coins iou-note))))
+  (defparameter bag '((description "a paper bag") (contents (sandwich apple soda))))
+  (defparameter box `((description "small blue box") (contents (,pouch feather))))
+  (defparameter backpack `((description "leather backpack") (contents (,box ,bag)))))
+
