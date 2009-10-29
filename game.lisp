3434d56aba9671b906ccb3e71c1d3979c98b510b
Jason Axelson (dell.server) <jason.axelson@gmail.com>
2009-10-28 23:11:46 -1000

    game: added macros

--- game.lisp
+++ game.lisp
@@ -3,11 +3,21 @@
   (/ (length (symbol-plist obj))
      2))
 
+(defmacro num-propsm (obj)
+  `(/ (length (symbol-plist (quote ,obj)))
+     2))
+
 (defun get-prop (obj property)
   (get obj property))
 
+(defmacro get-propm (obj property)
+  `(get (quote ,obj) ,property))
+
 (defun set-prop (obj property value)
   (setf (get obj property) value))
+
+(defmacro set-propm (obj property value)
+  `(setf (get (quote ,obj) ,property) ,value))
 
 (defun prop-with-value (obj value)
   (let ((position (search (list value) (symbol-plist obj))))
