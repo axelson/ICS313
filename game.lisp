aaffe6c875e1bd6ce195af2b6e94e0e45b20493d
Jason Axelson (arch-dell-laptop) <jason.axelson@gmail.com>
2009-11-02 13:01:42 -1000

    Finished everything except for new-prop

--- game.lisp
+++ game.lisp
@@ -32,7 +32,7 @@
     ((containerp obj)
      (loop for item in (cadadr obj)
         do
-                                        ;(format t "item: ~A~%" item)
+        ;;(format t "item: ~A~%" item)
         (cond
           ((containerp item)
            (if (contains? item itemname)
@@ -40,6 +40,45 @@
           ((eql item itemname)
            (return t))))
      )))
+
+(setf bart    
+      (cons (list
+             'talk #'(lambda ()
+                       (format t "Don't have a cow, man!~%")))
+            bart))
+
+;; Write a function or macro (do-action obj action) which will call the property's function, and test it by making bart talk. Hint: you may want to use funcall.
+(defun do-action (obj action)
+  (funcall (get-prop obj action)))
+
+(set-prop bart 'talk
+          #'(lambda (obj)
+              (let ((age (get-prop obj 'age)))
+                (cond
+                  ((< age 10) (format t "Don't have a cow, man!~%"))
+                  ((<= age 18) (format t "Render unto me a fracture!~%"))
+                  (t (format t "Are we having fun yet?~%"))))))
+
+(defun do-action (obj action)
+  (funcall (get-prop obj action) obj))
+
+(setf
+ bart
+ (cons (list
+        'update #'(lambda (obj)
+                    (set-prop obj 'age
+                              (1+ (get-prop obj 'age)))
+                    (do-action obj 'talk)))
+       bart))
+
+(defun run-bart ()
+  (loop
+     initially (set-prop bart 'age 7)
+     do (do-action bart 'update)
+     until (= (get-prop bart 'age) 21)))
+
+(defmacro add-prop (symbol name default-value)
+  `(setf ,symbol (cons (list ,name ,default-value) ,symbol)))
 
 
 ; If using plists
@@ -52,6 +91,8 @@
 (defparameter bart '((name "Bart Simpson")
                      (age 7)
                      (hair-color yellow)))
+
+
 
 (progn
   (defparameter pouch '((description "a small coin pouch") (contents (coins iou-note))))
